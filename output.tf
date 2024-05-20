65248b9cbb587f27ba178922a5e14fbcfec57454
Dustin Scott <dscott@redhat.com>
2024-05-20 12:05:02 -0500

    feat: output service principal data as outputs
    This allows the service principal outputs to be consumed by other automation.
    
    Signed-off-by: Dustin Scott <dscott@redhat.com>

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@
 # output created service principals and credentials to a file
 #
 resource "local_sensitive_file" "cluster_service_principal" {
-  count = var.cluster_service_principal.create ? 1 : 0
+  count = var.cluster_service_principal.create && var.output_as_file ? 1 : 0
 
   content         = <<-EOT
 ARO_CLUSTER_SP_CLIENT_ID='${azuread_application.cluster[0].client_id}'
@@ -13,7 +13,7 @@ EOT
 }
 
 resource "local_sensitive_file" "installer_service_principal" {
-  count = local.installer_user_set ? 0 : ((var.installer_service_principal.create) ? 1 : 0)
+  count = local.installer_user_set ? 0 : ((var.installer_service_principal.create && var.output_as_file) ? 1 : 0)
 
   content         = <<-EOT
 ARO_INSTALLER_SP_CLIENT_ID='${azuread_application.installer[0].client_id}'
@@ -22,4 +22,29 @@ ARO_TENANT_ID='${data.azuread_client_config.current.tenant_id}'
 EOT
   filename        = "./${var.cluster_name}_installer-sp-credentials.txt"
   file_permission = "0600"
+}
+
+locals {
+  cluster_service_principal_client_id       = var.cluster_service_principal.create ? azuread_application.cluster[0].client_id : null
+  cluster_service_principal_client_secret   = var.cluster_service_principal.create ? azuread_application_password.cluster[0].value : null
+  installer_service_principal_client_id     = var.cluster_service_principal.create ? azuread_application.installer[0].client_id : null
+  installer_service_principal_client_secret = var.cluster_service_principal.create ? azuread_application_password.installer[0].value : null
+}
+
+output "cluster_service_principal_client_id" {
+  value = local.cluster_service_principal_client_id
+}
+
+output "cluster_service_principal_client_secret" {
+  value     = local.cluster_service_principal_client_secret
+  sensitive = true
+}
+
+output "installer_service_principal_client_id" {
+  value = local.installer_service_principal_client_id
+}
+
+output "installer_service_principal_client_secret" {
+  value     = local.installer_service_principal_client_secret
+  sensitive = true
 }
